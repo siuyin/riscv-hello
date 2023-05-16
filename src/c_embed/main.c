a6570732bd180c566f3a7923113be13f9ae0d7bc
Loh Siu Yin <siuyin@beyondbroadcast.com>
2023-05-16 12:48:14 +0800

    Updated article.

--- src/c_embed/main.c
+++ src/c_embed/main.c
@@ -4,14 +4,15 @@ typedef unsigned char uint8_t;
 #define UART_BASE ((size_t *)0x10013000)
 #define TXDATA (UART_BASE + 0)
 
-static int putchar(uint8_t ch) {
+// Making putchar static optimizes space as it will not be 'exported'
+// to be visible in other source files.
+static void putchar(uint8_t ch) {
 	while ((volatile int)*TXDATA < 0);
 	*TXDATA = ch;
-	
-	return (int)ch;
 }
 
 void puts(char *s) {
+	// while char pointed to by s is non-zero, putchar and increment the pointer
 	while (*s) putchar(*s++);
 	putchar('\n');
 }
